346d2bfdda3dc1e97e1fbffd470589f0fd8491b1
bileeave <jubileegavenido@gmail.com>
2023-12-29 19:00:47 +0800

    Add initial part of styling

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const item = document.createElement("div");
+item.setAttribute("class", "item");
+container.appendChild(item);
+const display = document.createElement("div");
+display.setAttribute("class", "display");
+item.appendChild(display);
