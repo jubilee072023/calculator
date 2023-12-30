e4d477b6187a5534ec7058eaca42a91c9d4f8ba4
bileeave <jubileegavenido@gmail.com>
2023-12-31 00:17:41 +0800

    Add keys and comment the todo's

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ container.appendChild(item);
 const display = document.createElement("div");
 display.setAttribute("class", "display");
 item.appendChild(display);
+const keys = document.createElement("div");
+keys.setAttribute("class", "keys");
+item.appendChild(keys);
+
+// i want to add div keys
+// but enclose them all in an event listener
+// with switch case statements, like in javascript.info
+// i also need separate functions to do the actions
